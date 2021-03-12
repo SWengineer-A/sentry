f80ccbe2b046d3e2af632333449d4989e0be1435
fullstack702 <valleyball.star@yandex.com>
2021-03-12 11:12:43 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+    dsn: "https: //4d80c47cf20b41e98082f4f24b262c61@o549363.ingest.sentry.io/5672059",
+    environment: process.env.NODE_ENV
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
